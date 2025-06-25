a6442552a3ce667c440bddc5e6c4d64831dbbf37
losarus <losarus@gmail.com>
2025-06-25 11:57:58 +0300

    Initial Commit

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+"""
+
 # This is a sample Python script.
 
 # Press Shift+F10 to execute it or replace it with your code.
@@ -14,3 +16,22 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+"""
+
+promt = ('Витязь на распутье')
+'Налево (L) пойдешь, вольну-волю обретешь...'
+'Направо (R) пойдешь, коня потеряешь...'
+'Прямо (F) пойдешь, сыт и весел будешь...'
+print (promt)
+choice = input('Куда идем (L, R или F): ')
+if choice == 'L' or choice == 'l':
+    print ('Поздравляю свободного гражданина!')
+elif choice == 'R' or choice == 'r':
+    print ('Мне жаль Вашего коня, рыцарь!')
+elif choice == 'F' or choice == 'f':
+    print('Раз пошла такая пьянка, режь последний огурец!')
+else:
+    print('Давай уже куда-нибудь пойдем!')
+
+
