3b4dd9886890484d440adaf3784b8d2121a55e76
losarus <losarus@gmail.com>
2025-07-10 13:05:29 +0300

    first_commit

--- oop.py
+++ oop.py
@@ -19,11 +19,69 @@ issubclass() Car:
     def drive_(self, place):
         if self.engine_on:
 
-"""
 
 class Sorter:
     def __init__(self):
         self.words =[]
 
-    def
+
+
+
+# Полиморфизм
+# method override; operator (overloading)
+
+print(1 + 2)
+print(1 + 2.0)
+print('abc' + 'def')
+print([1, 2] + [3, 4])
+
+def func(x, y):
+    return x + y
+
+print(func(x,Y))
+
+"""
+
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self._author = author
+
+    def get_title(self):
+        return self.title
+
+    def get_author(self):
+        return self._author
+
+book = Book('Язык С++', 'Бьярн Страупступ')
+print(f'{book.get_title(), book.get_author()}')
+
+from math import pi
+
+class Circle:
+    def __init__(self):(, radius):
+    self.radius = radius
+
+    def perimetr(self):
+        return 2 * pi * self.radius
+
+    def area(self):
+        return 2* pi * self.radius
+
+
+class Square:
+
+    def __init__(self):(, high. widht)
+        self.
+
+
+# "Утиная" типизация в Python
+#
+#
+
+
+
+
+
+
 
